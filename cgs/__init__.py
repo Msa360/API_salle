c2b1e43b063fa925e65d66cb20505bfd372d4d1b
Msa360 <arnaud25@icloud.com>
2023-01-10 11:25:48 -0500

    fixed gh actions

--- cgs/__init__.py
+++ cgs/__init__.py
@@ -3,5 +3,5 @@ from .updateres import login_update
 from .fullall import reserve_all as killswitch
 
 
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 __all__ = ["login_create", "login_update", "killswitch"]
